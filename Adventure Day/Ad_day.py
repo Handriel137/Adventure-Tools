d9cc881858fbb2028ea6c5654e72bda2bc266b96
Handriel137 <r.solnik@gmail.com>
2018-11-25 20:26:28 -0500

    working encounter building

--- Adventure Day/Ad_day.py
+++ Adventure Day/Ad_day.py
@@ -6,6 +6,8 @@ import json
 import re
 import psycopg2
 import mistune
+import random
+
 
 # REGEX
 # re_header = re.compile("^={1,2} ([\\w\\s]+) ={1,2}")
@@ -243,7 +245,7 @@ def encounterQuery(CR, location):
     conn = None
     try:
 
-        monster_list = {}
+        monster_list = []
 
         # connect to the PostgreSQL server
         print(f'****************************************************')
@@ -261,32 +263,23 @@ def encounterQuery(CR, location):
         
 
         i=0
-        
-        
+                
         while i < len(data):
             # print(type(data[i][0]))
 
             # monster_list.update()
             monster = data[i][0]
             # print(monster['name'])
-            monster_list[monster['name']] = monster
+            monster_list.append(monster)
             i += 1
 
         # printing all of the monster names found.
-        # print(monster_list.keys())
-        for key in monster_list.keys():
-            print(monster_list[key]['name'])
+        # for key in monster_list.keys():
+        #     print(monster_list[key]['name'])
 
+        return monster_list
         #strips the list and touple format
         # return data[0]
-        
- 
-        # display the PostgreSQL database server version
-        # execute a statement
-        # print('PostgreSQL database version:')
-        # cur.execute('SELECT version()')
-        # db_version = cur.fetchone()
-        # print(db_version)
        
         # close the communication with the PostgreSQL
         cur.close()
@@ -298,7 +291,10 @@ def encounterQuery(CR, location):
             conn.close()
             # print('Database connection closed.')
 
-def buildEncounter(level, players, location):
+
+
+
+def buildEasyEncounter(level, players, location):
     
     if(type(level) is not int):
         print("changing to integer")
@@ -309,6 +305,8 @@ def buildEncounter(level, players, location):
     hard_xp = [0,75,150,225,375,750,900,1100,1400,1600,1900,2400,3000,3400,3800,4300,4800,5900,6300,7300,8500]
     deadly_xp = [0,100,200,400,500,1100,1400,1700,2100,2400,2800,3600,4500,5100,5700,6400,7200,8800,9500,10900,12700]
 
+    encounter_mulitplier = {'1':1, '2': 1.5, '3': 2, '4': 2, '5': 2, '6': 2, '7': 2.5, '8': 2.5,  '9': 2.5,  '10': 2.5,  '11': 3, '12': 3, '13': 3,  '14': 3, '15': 4, '16':4, '17': 4}
+
     # determine easy XP threshold
     easy_xp_allotment = players * easy_xp[level]
     medium_xp_allotment = players * medium_xp[level]
@@ -316,13 +314,275 @@ def buildEncounter(level, players, location):
     deadly_xp_allotment = players * deadly_xp[level]    
     print(easy_xp_allotment)
     easy_monsters = encounterQuery(level, location)
+
+    # building an encounter
+    encounter_xp = 0
+    encounter_roster = []
+    easy_encounter_monsters = easy_monsters.copy()
+    while encounter_xp < easy_xp_allotment or encounter_xp <= medium_xp_allotment:
+        
+        #limits the number of different monsters
+        if(len(encounter_roster) > 4):
+            easy_encounter_monsters = encounter_roster
+
+        # Picks a monster
+        encounter_monster = random.choice(list(easy_encounter_monsters))
+        xp = encounter_monster['experience']
+
+        #monster xp is 80% of allotment and xp left in allotment
+        if(xp <= (medium_xp_allotment/2) and xp <= medium_xp_allotment - encounter_xp):
+            
+            encounter_roster.append(encounter_monster)
+            # print(f"added {encounter_monster['name']}")
+            # print(f"{encounter_monster['experience']}")
+            # print(len(encounter_roster))
+            
+            encounter_xp = 0
+            
+            #adds monster to the list for the encounter
+            for monster in encounter_roster:
+                encounter_xp += monster['experience']
+
+            #calculates the experience multiplier based on # of monsters
+            encounter_xp = encounter_xp * encounter_mulitplier[f'{len(encounter_roster)}']
+        
+        #if between the easy and medium range stop looking for monsters to add
+        if(encounter_xp in range(easy_xp_allotment, medium_xp_allotment)):
+            break
+
+    print("Encounter Roster\n")
+    for monster in encounter_roster:
+        print(monster['name'])
+
+    print("encounter xp")
+    print(encounter_xp)
     # print(easy_monsters)
-    print(medium_xp_allotment)
-    print(hard_xp_allotment)
-    print(deadly_xp_allotment)
+    # for key in easy_monsters.keys():
+    #     print(f"{easy_monsters[key]['name']} - {easy_monsters[key]['experience']}")
+    # print(medium_xp_allotment)
+    # print(hard_xp_allotment)
+    # print(deadly_xp_allotment)
 
+def buildMediumEncounter(level, players, location):
+    
+    if(type(level) is not int):
+        print("changing to integer")
+        level = int(level)
 
+    easy_xp = [0,25,50,75,125,250,300,350,450,550,600,800,1000,1100,1250,1400,1600,2000,2100,2400,2800]
+    medium_xp = [0,50,100,150,250,500,600,750,900,1100,1200,1600,2000,2200,2500,2800,3200,3900,4200,4900]
+    hard_xp = [0,75,150,225,375,750,900,1100,1400,1600,1900,2400,3000,3400,3800,4300,4800,5900,6300,7300,8500]
+    deadly_xp = [0,100,200,400,500,1100,1400,1700,2100,2400,2800,3600,4500,5100,5700,6400,7200,8800,9500,10900,12700]
 
+    encounter_mulitplier = {'1':1, '2': 1.5, '3': 2, '4': 2, '5': 2, '6': 2, '7': 2.5, '8': 2.5,  '9': 2.5,  '10': 2.5,  '11': 3, '12': 3, '13': 3,  '14': 3, '15': 4, '16':4, '17': 4}
+
+    # determine easy XP threshold
+    easy_xp_allotment = players * easy_xp[level]
+    medium_xp_allotment = players * medium_xp[level]
+    hard_xp_allotment = players * hard_xp[level]
+    deadly_xp_allotment = players * deadly_xp[level]    
+    print(easy_xp_allotment)
+    easy_monsters = encounterQuery(level, location)
+
+    # building an encounter
+    encounter_xp = 0
+    encounter_roster = []
+    easy_encounter_monsters = easy_monsters.copy()
+    while encounter_xp < medium_xp_allotment or encounter_xp <= hard_xp_allotment:
+        
+        #limits the number of different monsters
+        if(len(encounter_roster) > 4):
+            easy_encounter_monsters = encounter_roster
+
+        # Picks a monster
+        encounter_monster = random.choice(list(easy_encounter_monsters))
+        xp = encounter_monster['experience']
+
+        #monster xp is 80% of allotment and xp left in allotment
+        if(xp <= hard_xp_allotment /2 and xp <= hard_xp_allotment - encounter_xp):
+            
+            encounter_roster.append(encounter_monster)
+            # print(f"added {encounter_monster['name']}")
+            # print(f"{encounter_monster['experience']}")
+            # print(len(encounter_roster))
+            
+            encounter_xp = 0
+            
+            #adds monster to the list for the encounter
+            for monster in encounter_roster:
+                encounter_xp += monster['experience']
+
+            #calculates the experience multiplier based on # of monsters
+            encounter_xp = encounter_xp * encounter_mulitplier[f'{len(encounter_roster)}']
+        
+        #if between the easy and medium range stop looking for monsters to add
+        if(encounter_xp in range(medium_xp_allotment, hard_xp_allotment)):
+            break
+
+    print("Encounter Roster\n")
+    for monster in encounter_roster:
+        print(f"{monster['name']} - {monster['experience']}")
+
+    print("encounter xp")
+    print(encounter_xp)
+    # print(easy_monsters)
+    # for key in easy_monsters.keys():
+    #     print(f"{easy_monsters[key]['name']} - {easy_monsters[key]['experience']}")
+    # print(medium_xp_allotment)
+    # print(hard_xp_allotment)
+    # print(deadly_xp_allotment)
+
+def buildHardEncounter(level, players, location):
+    
+    if(type(level) is not int):
+        print("changing to integer")
+        level = int(level)
+
+    easy_xp = [0,25,50,75,125,250,300,350,450,550,600,800,1000,1100,1250,1400,1600,2000,2100,2400,2800]
+    medium_xp = [0,50,100,150,250,500,600,750,900,1100,1200,1600,2000,2200,2500,2800,3200,3900,4200,4900]
+    hard_xp = [0,75,150,225,375,750,900,1100,1400,1600,1900,2400,3000,3400,3800,4300,4800,5900,6300,7300,8500]
+    deadly_xp = [0,100,200,400,500,1100,1400,1700,2100,2400,2800,3600,4500,5100,5700,6400,7200,8800,9500,10900,12700]
+
+    encounter_mulitplier = {'1':1, '2': 1.5, '3': 2, '4': 2, '5': 2, '6': 2, '7': 2.5, '8': 2.5,  '9': 2.5,  '10': 2.5,  '11': 3, '12': 3, '13': 3,  '14': 3, '15': 4, '16':4, '17': 4}
+
+    # determine easy XP threshold
+    easy_xp_allotment = players * easy_xp[level]
+    medium_xp_allotment = players * medium_xp[level]
+    hard_xp_allotment = players * hard_xp[level]
+    deadly_xp_allotment = players * deadly_xp[level]    
+    print(easy_xp_allotment)
+    easy_monsters = encounterQuery(level, location)
+
+    # building an encounter
+    encounter_xp = 0
+    encounter_roster = []
+    easy_encounter_monsters = easy_monsters.copy()
+    while encounter_xp < hard_xp_allotment or encounter_xp <= deadly_xp_allotment:
+        
+        #limits the number of different monsters
+        if(len(encounter_roster) > 4):
+            easy_encounter_monsters = encounter_roster
+
+            
+
+        # Picks a monster
+        encounter_monster = random.choice(list(easy_encounter_monsters))
+        xp = encounter_monster['experience']
+        
+        print(len(encounter_roster))
+
+        while(len(encounter_roster) is 0 and encounter_monster['challenge_rating'] < level / 2):
+            encounter_monster = random.choice(list(easy_encounter_monsters))
+            xp = encounter_monster['experience']
+            print("changed monster")
+        
+        #monster xp is 80% of allotment and xp left in allotment
+        if(xp <= (deadly_xp_allotment*.8) and xp <= deadly_xp_allotment - encounter_xp):
+            
+            encounter_roster.append(encounter_monster)
+            # print(f"added {encounter_monster['name']}")
+            # print(f"{encounter_monster['experience']}")
+            # print(len(encounter_roster))
+            
+            encounter_xp = 0
+            
+            #adds monster to the list for the encounter
+            for monster in encounter_roster:
+                encounter_xp += monster['experience']
+
+            #calculates the experience multiplier based on # of monsters
+            encounter_xp = encounter_xp * encounter_mulitplier[f'{len(encounter_roster)}']
+        
+        #if between the easy and medium range stop looking for monsters to add
+        if(encounter_xp in range(hard_xp_allotment, deadly_xp_allotment)):
+            break
+
+    print("Encounter Roster\n")
+    for monster in encounter_roster:
+        print(f"{monster['name']} - {monster['experience']}")
+
+    print("encounter xp")
+    print(encounter_xp)
+    # print(easy_monsters)
+    # for key in easy_monsters.keys():
+    #     print(f"{easy_monsters[key]['name']} - {easy_monsters[key]['experience']}")
+    # print(medium_xp_allotment)
+    # print(hard_xp_allotment)
+    # print(deadly_xp_allotment)
+
+def buildDeadlyEncounter(level, players, location):
+    
+    if(type(level) is not int):
+        print("changing to integer")
+        level = int(level)
+
+    easy_xp = [0,25,50,75,125,250,300,350,450,550,600,800,1000,1100,1250,1400,1600,2000,2100,2400,2800]
+    medium_xp = [0,50,100,150,250,500,600,750,900,1100,1200,1600,2000,2200,2500,2800,3200,3900,4200,4900]
+    hard_xp = [0,75,150,225,375,750,900,1100,1400,1600,1900,2400,3000,3400,3800,4300,4800,5900,6300,7300,8500]
+    deadly_xp = [0,100,200,400,500,1100,1400,1700,2100,2400,2800,3600,4500,5100,5700,6400,7200,8800,9500,10900,12700]
+
+    encounter_mulitplier = {'1':1, '2': 1.5, '3': 2, '4': 2, '5': 2, '6': 2, '7': 2.5, '8': 2.5,  '9': 2.5,  '10': 2.5,  '11': 3, '12': 3, '13': 3,  '14': 3, '15': 4, '16':4, '17': 4}
+
+    # determine easy XP threshold
+    easy_xp_allotment = players * easy_xp[level]
+    medium_xp_allotment = players * medium_xp[level]
+    hard_xp_allotment = players * hard_xp[level]
+    deadly_xp_allotment = players * deadly_xp[level]    
+    print(easy_xp_allotment)
+    easy_monsters = encounterQuery(level, location)
+
+    # building an encounter
+    encounter_xp = 0
+    encounter_roster = []
+    easy_encounter_monsters = easy_monsters.copy()
+    while encounter_xp <= deadly_xp_allotment:
+        
+        #limits the number of different monsters
+        if(len(encounter_roster) > 4):
+            easy_encounter_monsters = encounter_roster
+
+        # Picks a monster
+        encounter_monster = random.choice(list(easy_encounter_monsters))
+        xp = encounter_monster['experience']
+
+        while(len(encounter_roster) is 0 and encounter_monster['challenge_rating'] < level-2):
+            encounter_monster = random.choice(list(easy_encounter_monsters))
+            xp = encounter_monster['experience']
+            print(f"{encounter_monster['name']} - {encounter_monster['experience']}")
+
+        #monster xp is 80% of allotment and xp left in allotment
+        if(xp <= (deadly_xp_allotment/2) and xp <= deadly_xp_allotment - encounter_xp):
+            
+            encounter_roster.append(encounter_monster)
+            # print(f"added {encounter_monster['name']}")
+            # print(f"{encounter_monster['experience']}")
+            # print(len(encounter_roster))
+            
+            encounter_xp = 0
+            
+            #adds monster to the list for the encounter
+            for monster in encounter_roster:
+                encounter_xp += monster['experience']
+
+            #calculates the experience multiplier based on # of monsters
+            encounter_xp = encounter_xp * encounter_mulitplier[f'{len(encounter_roster)}']
+        
+        #if between the easy and medium range stop looking for monsters to add
+        if(encounter_xp in range(deadly_xp_allotment, (deadly_xp_allotment*2))):
+            break
+
+    print("Encounter Roster\n")
+    for monster in encounter_roster:
+        print(monster['name'])
+
+    print("encounter xp")
+    print(encounter_xp)
+    # print(easy_monsters)
+    # for key in easy_monsters.keys():
+    #     print(f"{easy_monsters[key]['name']} - {easy_monsters[key]['experience']}")
+    # print(medium_xp_allotment)
+    # print(hard_xp_allotment)
+    # print(deadly_xp_allotment)
 # TODO create template for monster in markdown
 def monsterMarkdown(monster):
     print("    ___")
@@ -396,7 +656,7 @@ if __name__ == '__main__':
             adventure_level = adventure_level[0]
             print(adventure_level)
             print("Building an encounter")
-            buildEncounter(adventure_level, players, 'forest')
+            buildDeadlyEncounter(adventure_level, players, 'forest')
 
         if(tag not in entry_list):
             result = monsterQuery(tag)
